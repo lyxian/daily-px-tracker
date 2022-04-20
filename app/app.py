dc21701950b77827cde1dbcece5462b56b9f32dd
lyx <yxianlai@hotmail.com>
2022-04-20 21:11:41 +0800

    v2.1 - add volume datatable, center-align

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
+from math import remainder
 from dash import Dash, html, dash_table, dcc, Output, Input
 from plotly import express, graph_objects, subplots
 import pandas
 import sys
 import os
+import re
 
 if len(sys.argv) == 2:
     MARKET = sys.argv[1]
@@ -10,13 +12,32 @@ else:
     print('Please input market as first input..')
     sys.exit()
 
+INTERVAL = 60
 defaultDirectory = f'data/{MARKET}'
 defaultStock = sorted(os.listdir(f'{defaultDirectory}'))[0]
 defaultFileName = sorted(os.listdir(f'{defaultDirectory}/{defaultStock}'))[-1]
 FILENAME = f'{defaultDirectory}/{defaultStock}/{defaultFileName}' 
 df = pandas.read_csv(FILENAME)
 
+volume_df = df.loc[:, ['datetime', 'volume']]
+volume_df.loc[:, 'datetime'] = df['datetime'].apply(lambda x: x.split()[1])
+
 if __name__ == '__main__':
+
+    def generateHtmlTables(df, interval):
+        num = df.shape[0] // interval
+        return [
+            html.Div(children=[
+                dash_table.DataTable(
+                    id=f'table{i}',
+                    style_cell={'textAlign': 'center'},
+                    # style_cell={'overflow': 'hidden', 'textOverflow': 'ellipsis', 'maxWidth': '50px'},
+                    style_data={'whiteSpace': 'normal', 'height': 'auto', 'lineHeight': '15px'},
+                    data=df.loc[i*interval:(i+1)*interval,].to_dict('records'),
+                    columns=[{"name": col, "id": col} for col in df.columns]
+                )
+            ], style={'display': 'inline-block', 'width': f'{100//num}%'}) for i in range(num)
+        ]
 
     app = Dash(__name__)
 
@@ -61,8 +82,9 @@ if __name__ == '__main__':
             html.Div(children = [
                 dcc.Graph(id=f'img_1', figure=img_candlestick, style={'height': 750}),
                 dcc.Graph(id=f'img_2', figure=img_volume, style={'height': 250})
-            ])
-        ])
+            ]),
+            *generateHtmlTables(volume_df, INTERVAL),
+        ], style={'text-align': 'center'})
     else:
         app.layout = html.Div(children = [
             html.Div(children = [
